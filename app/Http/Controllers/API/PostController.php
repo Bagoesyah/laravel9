5d621dc6ae06aefae0d5d72ea741278467831120
Bagoesyah <bnurmansyah8@gmail.com>
2022-08-11 11:46:32 +0700

    update beta

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
 
         $validate = Validator::make($request->all(), [
             'title' => 'required',
-            'url_key' => 'unique:post,author',
+            'url_key' => 'required|unique:post,url_key|alpha_dash',
             'content' => 'required',
         ]);
 
@@ -27,18 +27,24 @@ class PostController extends Controller
             ]);
         }
 
-        $Post = new Post;
-        $Post->title = $request->title;
-        $Post->url_key = str::slug($request->url_key);
-        $Post->content = $request->content;
-        $Post->author = $request->author === null ? Auth::user()->name : $request->author;
+        $post = Post::create([
+            "title" => $request->title,
+            "url_key" => $request->url_key,
+            "content" => $request->content,
+            "author" => $request->author === null ? Auth::user()->name : $request->author,
+        ]);
+        // $post = new Post;
+        // $post->title = $request->title;
+        // $post->url_key = str::slug($request->url_key);
+        // $post->content = $request->content;
+        // $post->author = $request->author === null ? Auth::user()->name : $request->author;
 
-        $Post->save();
+        // $post->save();
 
         return response()->json(
             [
                 "message" => "success",
-                "data" => $Post
+                "data" => $post
             ]
         );
     }
@@ -50,6 +56,63 @@ class PostController extends Controller
         $limit = $request->limit;
         $data = Post::paginate($limit);
         return $data;
+    }
+
+    //Post Detail
+    public function show($id) {
+        $post = Post::find($id);
+
+        return response()->json(
+            [
+                "message" => "success",
+                "data" => $post
+            ]
+        );
+    }
+
+    //Post Update
+    public function edit($id, Request $request) {
+        $post = Post::where('id', $id)->first();
+        if($post){
+            $post->title = $request->title ? $request->title : $post->title;
+            $post->content = $request->content ? $request->content : $post->content;
+            $post->author = $request->author ? $request->author : $post->author;
+
+            $post->save();
+            return response()->json(
+                [
+                    "message" => "update " . $id . " success",
+                    "data" =>  $post
+                ]
+            );
+        }
+        else {
+            return response()->json(
+                [
+                    "message" => "post with id " . $id . " not found"
+                ], $status = 400
+            );
+        }
+    }
+
+    //Post Delete
+    public function delete($id) {
+        $post = Post::where('id', $id)->first();
+        if($post){
+            $post->delete();
+            return response()->json(
+                [
+                    "message" => "delete product id " . $id . " success"
+                ]
+            );
+        }
+        else{
+            return response()->json(
+                [
+                    "message" => "post with id " . $id . " not found"
+                ], $status = 400
+            );
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -26,66 +26,9 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::group(["middleware" => "auth:sanctum"], function () {
-    Route::post('/posts/create', [PostController::class, 'post']);});
-
-
-//show all
-Route::get('/posts', [PostController::class, 'get']);
-
-//show detail
-Route::get('/posts/{id}', function (Post $id) {
-    $post = Post::find($id);
-
-    return response()->json(
-        [
-            "message" => "success",
-            "data" => $post
-        ]
-    );
+    Route::get('/posts', [PostController::class, 'get']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::post('/posts/create', [PostController::class, 'post']);
+    Route::put('/posts/{id}', [PostController::class, 'edit']);
+    Route::delete('/posts/{id}', [PostController::class, 'delete']);
 });
-
-//update
-Route::put('/posts/{id}', function ($id, Request $request) {
-    $post = Post::where('id', $id)->first();
-    if($post){
-        $post->title = $request->title ? $request->title : $post->title;
-        $post->content = $request->content ? $request->content : $post->content;
-        $post->author = $request->author ? $request->author : $post->author;
-
-        $post->save();
-        return response()->json(
-            [
-                "message" => "update " . $id . " success",
-                "data" =>  $post
-            ]
-        );
-    }
-    else {
-        return response()->json(
-            [
-                "message" => "post with id " . $id . " not found"
-            ], $status = 400
-        );
-    }
-});
-
-//delete
-Route::delete('/posts/{id}', function ($id) {
-    $post = Post::where('id', $id)->first();
-    if($post){
-        $post->delete();
-        return response()->json(
-            [
-                "message" => "delete product id " . $id . " success"
-            ]
-        );
-    }
-    else{
-        return response()->json(
-            [
-                "message" => "post with id " . $id . " not found"
-            ], $status = 400
-        );
-    }
-});
-
